65034ec64343a62b3ce3ad092aa7b72409659bd7
Mikita Lisavets <mikita.lisavets@gmail.com>
2015-09-11 11:25:27 +0200

    Update blog

--- gulpfile.js
+++ gulpfile.js
@@ -92,7 +92,7 @@ var remote = 'https://github.com/FeelEnergyCB/blog.git',
 
   function rePush(err) {
     if (err) {
-      console.log(err);
+      console.dir(err);
       git.push('origin', 'master', function(err) { rePush(err) } );
     }
   }
