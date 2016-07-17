b6d8f903110d8bbb910b9aba48f47e93de15a7a0
Mikita Lisavets <mikita.lisavets@gmail.com>
2016-07-17 10:57:08 +0300

    update blog

--- assets/scripts/js/boundle.js
+++ assets/scripts/js/boundle.js
@@ -69,6 +69,20 @@
     'use strict';
     angular
         .module('blog')
+        .filter('month', month);
+    function month() {
+        return monthFilter;
+        ////////////////
+        function monthFilter(items) {
+          
+          var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+          return months[items];
+      }
+    }
+})();;(function() {
+    'use strict';
+    angular
+        .module('blog')
         .directive('list', list);
     list.$inject = [];
     /* @ngInject */
@@ -101,20 +115,6 @@
     }
     /* @ngInject */
     function MainController() {
-    }
-})();;(function() {
-    'use strict';
-    angular
-        .module('blog')
-        .filter('month', month);
-    function month() {
-        return monthFilter;
-        ////////////////
-        function monthFilter(items) {
-          
-          var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-          return months[items];
-      }
     }
 })();;(function() {
     'use strict';
